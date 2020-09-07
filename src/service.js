8b469db3582a1036cf0149873e219ea3291458ee
dazza65x <dazza@me.com>
2020-09-08 06:08:10 +0800

    Modified ports for local docker-compose invocation. Added catch() for order service fetch.

--- src/service.js
+++ src/service.js
@@ -4,9 +4,9 @@ const fetch = require('node-fetch');
 const app = express(); 
 const this_service = "customerorders"
 const customerservice = process.env.CUSTOMER_SVC || "localhost"
-const customerserviceport = process.env.CUSTOMER_SVC_PORT || 9010
+const customerserviceport = process.env.CUSTOMER_SVC_PORT || 8080
 const orderservice = process.env.ORDER_SVC || "localhost"
-const orderserviceport = process.env.ORDER_SVC_PORT || 9020
+const orderserviceport = process.env.ORDER_SVC_PORT || 8080
 
 const port = 8080;
 
@@ -44,15 +44,26 @@ app.get(`/${this_service}/`, function(req,res){
         });
         res.send(orderList); 
       })
+      .catch((error) => {
+        let err = { "Error:" : "Failed to get order details." };
+        console.log(error.message);
+        res.status(503).send({
+          status: 503,
+          error: 'Failed to get order details'
+        });
+      })
     } else {
       res.send(orderList);
     }
 
   })
   .catch((error) => {
-    let err = { "Error:" : "Failed to get customer orders." };
+    let err = { "Error:" : "Failed to get customer details." };
     console.log(error.message);
-    res.status(503).send(JSON.stringify(err));
+    res.status(503).send({
+      status: 503,
+      error: 'Failed to get customer details'
+    });
   })
 });
 
